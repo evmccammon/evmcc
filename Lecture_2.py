697266bbbfe604cbf0364d90d8598543ed30c160
evmccammon <evmccammon@gmail.com>
2026-01-13 15:54:09 -0800

    Add files via upload

---
+++ Lecture_2.py
@@ -0,0 +1,20 @@
+print('Hello World!')
+shopping_list = ['apples', 'bread', 'carrots'] 
+print(shopping_list)
+print(shopping_list[-1])
+print (len(shopping_list))
+print(8%3)
+print(type(shopping_list))
+num = 5 
+num += 2
+print(num)
+print(type(7))
+print(type('7'))
+print(type(int('7')))
+print(int('7'))
+num = input("Enter your lucky number: ")
+print(num)
+print(type(num)) 
+new_num = int(num)
+print(new_num)
+print(type(new_num))
